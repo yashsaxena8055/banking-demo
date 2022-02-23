8078aaa04ae848e1fe68cbf2109cce996a8dc7a3
yashsaxena8055 <yash.saxena@credable.in>
2022-02-23 10:59:19 +0530

    Added spring-boot configuration for web application and set up a controller module!!

--- src/main/java/org/example/service/defaultCustomerService.java
+++ src/main/java/org/example/service/defaultCustomerService.java
@@ -2,27 +2,25 @@ package org.example.service;
 
 import org.example.model.Customer;
 import org.example.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Service("customerService")
 public class defaultCustomerService implements CustomerService {
 
-
+    @Autowired
     private CustomerRepository repository;
 
-    // setter injection
-    public void setCustomerRepository(CustomerRepository repository) {
-        this.repository = repository;
-    }
 
 
     public defaultCustomerService(){};
 //     below is dependency injection
 
-//    public defaultCustomerService(CustomerRepository repository) {
-//        this.repository = repository;
-//    }
+    public defaultCustomerService(CustomerRepository repository) {
+        this.repository = repository;
+    }
 
     public List<Customer> getAllCustomers() {
 
